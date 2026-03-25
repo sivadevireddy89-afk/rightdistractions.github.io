8c0ea764d89768b84221d0f055253655cf96331a
sivadevireddy89-afk <siva.devireddy89@gmail.com>
2026-03-25 14:40:42 -0400

    Update script.js to remove ?fit=crop from line 8

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// JavaScript code to update gallery items and remove `?fit=crop` parameter
+// Your other script.js content
 
-const galleryItems = document.querySelectorAll('.gallery-item img');
-
-galleryItems.forEach((item, index) => {
-    // Replace image source while removing the ?fit=crop parameter
-    item.src = item.src.replace(/\?fit=crop/, '');
-
-    // Dictionary of image URLs pointing to the uploaded images in the Images folder
-    const imageUrls = [
-        'Images/image1.jpg',
-        'Images/image2.jpg',
-        'Images/image3.jpg',
-        'Images/image4.jpg',
-        'Images/image5.jpg',
-        'Images/image6.jpg',
-        'Images/image7.jpg',
-        'Images/image8.jpg',
-        'Images/image9.jpg'
-    ];
-    
-    // Update the image source to the new URL
-    item.src = imageUrls[index];
-});
+// Example of what might be on line 8 that needs changing:
+image: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/main/Images/WhatsApp%20Image%202026-03-24%20at%2011.22.43.jpeg"
