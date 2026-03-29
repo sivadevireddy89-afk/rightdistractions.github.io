0b641c039554a21528a92e6d83ca3631bb2b1620
sivadevireddy89-afk <siva.devireddy89@gmail.com>
2026-03-28 21:44:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const galleryData = [
         title: "City Skyline", 
         category: "location",
         description: "London - Tower Bridge capturing the essence of modern city life.",
-        image: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/main/Images/WhatsApp%20Image%202026-03-24%20at%2011.22.43%20(1).jpeg"
+        image: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/main/Images/WhatsApp%20Image%202026-03-24%20at%2011.22.43%20(1).jpeg",
         video: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/c91d16ee95352cad71edd05964511d4e67fb1362/Videos/WhatsApp%20Video%202026-03-28%20at%2021.05.26.mp4"
     },
     {
